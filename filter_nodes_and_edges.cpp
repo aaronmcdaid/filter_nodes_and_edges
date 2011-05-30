3e09f28e137893b6396e5ef54f8aeb9446fb7eb2
Aaron McDaid <aaronmcdaid@erdos.ucd.ie>
2011-05-30 03:09:48 +0100

    test the command line args

--- filter_nodes_and_edges.cpp
+++ filter_nodes_and_edges.cpp
@@ -1,7 +1,23 @@
+#include "cmdline.h"
 #include <iostream>
+#include <cstdlib>
+#include "pp.hpp"
 
 using namespace std;
 
-int main() {
+int main(int argc, char **argv) {
+	gengetopt_args_info args_info;
+
+	// there shouldn't be any errors in processing args
+	if (cmdline_parser (argc, argv, &args_info) != 0)
+		exit(1) ;
+	// .. and there should be exactly one non-option arg
+	if(args_info.inputs_num != 1) {
+		cmdline_parser_print_help();
+		exit(1);
+	}
+
+	const char * edgeListFileName   = args_info.inputs[0];
 	cout << "filter_nodes_and_edges: nothing implemented yet" << endl;
+	PP(edgeListFileName);
 }
